8382d46ef80c541f51694b9281be89df38b27f6f
Fukuta-Y <yufukuta1008@gmail.co.jp>
2023-06-15 00:42:09 +0900

    webclient1

--- src/main/java/com/example/demo/controller/ShukanTalentJohoSearchBFFController.java
+++ src/main/java/com/example/demo/controller/ShukanTalentJohoSearchBFFController.java
@@ -1,9 +1,12 @@
 package com.example.demo.controller;
 
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.client.RestTemplate;
 
 import com.api.ShukanTalentJohoSearchBFFApi;
+import com.model.ShukanTalentJohoSearch;
 import com.model.ShukanTalentJohoSearchBFF;
 
 import jakarta.validation.Valid;
@@ -15,22 +18,55 @@ import jakarta.validation.constraints.Size;
 @Controller
 public class ShukanTalentJohoSearchBFFController implements ShukanTalentJohoSearchBFFApi{
 
+//	@Autowired
+//	private WebTestClient webTestClient;
+
+	
+    private final RestTemplate restTempalte;
+	
+    
+    public ShukanTalentJohoSearchBFFController() {
+        this.restTempalte = new RestTemplate();
+    }
+    
+    private ShukanTalentJohoSearch getFirstDataByRestTemplate(Integer targetNentsuki,Integer targetShu, String talentName) {
+        return this.restTempalte
+                .getForObject(
+                String.format("http://localhost:8080/api/shukanTalentJohoSearchBFF?targetNentsuki={0}&targetShu={1}&talentName={2}",
+                targetNentsuki,targetShu,talentName), ShukanTalentJohoSearch.class);
+    }
+	
 	@Override
 	public ResponseEntity<ShukanTalentJohoSearchBFF> getShukanTalentJohoSearchBFF(
 			@NotNull @Min(199001) @Max(210012) @Valid Integer targetNentsuki,
 			@NotNull @Min(1) @Max(5) @Valid Integer targetShu, @Size(max = 30) @Valid String talentName) {
 		
+//		
+//		ShukanTalentJohoSearchBFF model = new ShukanTalentJohoSearchBFF();
+//
+//		model.setTalentId("");
+//		model.setTalentName("");
+//		model.setShukanShutsuenProgramHonsu(1);
+//		model.setShutsuenProgramChokin("dd");
+//		model.setOnAirDayChokin("");
+//		model.setShuFrom("");
+//		model.setShuTo("");
+		
+		System.out.println("URL:" + String.format("http://localhost:8080/api/shukanTalentJohoSearch?targetNentsuki=%d&targetShu=%d&talentName=%s",targetNentsuki,targetShu,talentName));
+		
+		ShukanTalentJohoSearch search = this.getFirstDataByRestTemplate(targetNentsuki,targetShu,talentName); // 2, 3
+		
+		System.out.print("Program:" + search.getmProgram());
+		System.out.print("Talent:" + search.getmTalent());
+		System.out.print("OnAirKanri:" + search.gettOnAirKanri());
+		
 		ShukanTalentJohoSearchBFF model = new ShukanTalentJohoSearchBFF();
-
-		model.setTalentId("");
-		model.setTalentName("");
-		model.setShukanShutsuenProgramHonsu(1);
-		model.setShutsuenProgramChokin("dd");
-		model.setOnAirDayChokin("");
-		model.setShuFrom("");
-		model.setShuTo("");
-
+		
 		return ResponseEntity.ok(model);
 	}
+	
+
+	
+    
 	
 }
--- src/main/java/com/example/demo/controller/ShukanTalentJohoSearchController.java
+++ src/main/java/com/example/demo/controller/ShukanTalentJohoSearchController.java
@@ -1,10 +1,14 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
 import com.api.ShukanTalentJohoSearchApi;
+import com.example.demo.dto.ProgramMasterDto;
 import com.example.demo.entity.ShukanTalentJohoSearchEntity;
 import com.example.demo.service.ShukanTalentJohoSearchService;
 import com.model.ShukanTalentJohoSearch;
@@ -35,10 +39,23 @@ public class ShukanTalentJohoSearchController implements ShukanTalentJohoSearchA
 		ShukanTalentJohoSearchEntity entity = service.select(targetNentsuki, targetShu, talentName);
 		
 		// TODO:MapStructの不備
-		//model.setmProgram(helper.toProgramModel(entity.getProgramMasterDto()));
-		//model.setmTalent(helper.toTalentModel(entity.getTalentMasterDto()));
-		//model.settOnAirKanri(helper.toOnAirKanriTableModel(entity.getOnAirKanriTableDto()));
+//		model.setmProgram(helper.toProgramModel(entity.getProgramMasterDto()));
+//		model.setmTalent(helper.toTalentModel(entity.getTalentMasterDto()));
+//		model.settOnAirKanri(helper.toOnAirKanriTableModel(entity.getOnAirKanriTableDto()));
+		
+		List<ProgramMasterDto>  programDto = entity.getProgramMasterDto();
 	
+		ProgramMasterDto dto = programDto.get(0);
+		
+		com.model.MProgram  program = new com.model.MProgram();
+		program.setChanelId(dto.getChanelId());
+		
+		List<com.model.MProgram>  listM = new ArrayList<com.model.MProgram>();
+		listM.add(program);
+		
+		model.setmProgram(listM);
+		
+		
 		return ResponseEntity.ok(model);
 	}
 }
