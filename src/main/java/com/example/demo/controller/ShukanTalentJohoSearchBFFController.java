d5b4827e50c3f7fc42bb926102004921e3c799fc
Fukuta-Y <yufukuta1008@gmail.co.jp>
2023-06-15 00:53:35 +0900

    webclientでBEへ接続成功時

--- src/main/java/com/example/demo/controller/ShukanTalentJohoSearchBFFController.java
+++ src/main/java/com/example/demo/controller/ShukanTalentJohoSearchBFFController.java
@@ -3,7 +3,7 @@ package com.example.demo.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.api.ShukanTalentJohoSearchBFFApi;
 import com.model.ShukanTalentJohoSearch;
@@ -14,26 +14,15 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import reactor.core.publisher.Mono;
 
 @Controller
 public class ShukanTalentJohoSearchBFFController implements ShukanTalentJohoSearchBFFApi{
 
-//	@Autowired
-//	private WebTestClient webTestClient;
+    private final WebClient webClient;
 
-	
-    private final RestTemplate restTempalte;
-	
-    
-    public ShukanTalentJohoSearchBFFController() {
-        this.restTempalte = new RestTemplate();
-    }
-    
-    private ShukanTalentJohoSearch getFirstDataByRestTemplate(Integer targetNentsuki,Integer targetShu, String talentName) {
-        return this.restTempalte
-                .getForObject(
-                String.format("http://localhost:8080/api/shukanTalentJohoSearchBFF?targetNentsuki={0}&targetShu={1}&talentName={2}",
-                targetNentsuki,targetShu,talentName), ShukanTalentJohoSearch.class);
+    public ShukanTalentJohoSearchBFFController(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.baseUrl("http://localhost:8080").build();
     }
 	
 	@Override
@@ -41,32 +30,24 @@ public class ShukanTalentJohoSearchBFFController implements ShukanTalentJohoSear
 			@NotNull @Min(199001) @Max(210012) @Valid Integer targetNentsuki,
 			@NotNull @Min(1) @Max(5) @Valid Integer targetShu, @Size(max = 30) @Valid String talentName) {
 		
-//		
-//		ShukanTalentJohoSearchBFF model = new ShukanTalentJohoSearchBFF();
-//
-//		model.setTalentId("");
-//		model.setTalentName("");
-//		model.setShukanShutsuenProgramHonsu(1);
-//		model.setShutsuenProgramChokin("dd");
-//		model.setOnAirDayChokin("");
-//		model.setShuFrom("");
-//		model.setShuTo("");
-		
-		System.out.println("URL:" + String.format("http://localhost:8080/api/shukanTalentJohoSearch?targetNentsuki=%d&targetShu=%d&talentName=%s",targetNentsuki,targetShu,talentName));
-		
-		ShukanTalentJohoSearch search = this.getFirstDataByRestTemplate(targetNentsuki,targetShu,talentName); // 2, 3
-		
-		System.out.print("Program:" + search.getmProgram());
-		System.out.print("Talent:" + search.getmTalent());
-		System.out.print("OnAirKanri:" + search.gettOnAirKanri());
-		
 		ShukanTalentJohoSearchBFF model = new ShukanTalentJohoSearchBFF();
+		
+		
+		Mono<ShukanTalentJohoSearch> search = this.getFirstDataByWebClient(targetNentsuki, targetShu, talentName);
+		
+		System.out.print("respose:" + search.block());
 		
 		return ResponseEntity.ok(model);
 	}
 	
 
-	
+	// BEへ接続
+    private Mono<ShukanTalentJohoSearch> getFirstDataByWebClient(Integer targetNentsuki, Integer targetShu, String talentName) {
+        return this.webClient.get()
+                .uri(String.format("/api/shukanTalentJohoSearch?targetNentsuki=%d&targetShu=%d&talentName=%s",targetNentsuki, targetShu, talentName))
+                .retrieve()
+                .bodyToMono(ShukanTalentJohoSearch.class);
+    }
     
 	
 }
