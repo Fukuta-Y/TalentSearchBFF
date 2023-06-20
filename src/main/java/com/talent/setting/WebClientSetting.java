9fc32323c6c65ff9109d1a0118e7bfd39618a5d8
Fukuta-Y <yufukuta1008@gmail.co.jp>
2023-06-20 13:41:25 +0900

    webclientの修正

--- src/main/java/com/talent/setting/WebClientSetting.java
+++ src/main/java/com/talent/setting/WebClientSetting.java
@@ -1,7 +1,7 @@
 package com.talent.setting;
 
 import org.springframework.stereotype.Service;
-// import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.model.ShukanTalentJohoSearch;
 import com.model.YearMonthWeekStartEndJoho;
