d55f64785be07a8b140ac5ccdb8753199267978c
Fukuta-Y <yufukuta1008@gmail.co.jp>
2023-06-24 14:18:51 +0900

    インデント

--- src/main/java/com/talent/service/dto/GroupClassDto4.java
+++ src/main/java/com/talent/service/dto/GroupClassDto4.java
@@ -7,9 +7,9 @@ import lombok.Data;
  */
 @Data
 public class GroupClassDto4 implements Serializable {
-	   private String talentId;
-	   private String talentName;
-	   private Long shukanShutsuenHonsu;
-	   private String programName;
-	   private String onairDay;
+   private String talentId;
+   private String talentName;
+   private Long shukanShutsuenHonsu;
+   private String programName;
+   private String onairDay;
 }
