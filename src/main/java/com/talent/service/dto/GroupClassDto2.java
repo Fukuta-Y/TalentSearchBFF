a4529c466cca5e31882ab21a77d4ad27e1530bc1
Fukuta-Y <yufukuta1008@gmail.co.jp>
2023-07-02 20:22:01 +0900

    体裁修正

--- src/main/java/com/talent/service/dto/GroupClassDto2.java
+++ src/main/java/com/talent/service/dto/GroupClassDto2.java
@@ -1,15 +1,17 @@
 package com.talent.service.dto;
-import java.io.Serializable;
-import com.talent.service.dto.entity.Talent;
 
+import com.talent.service.dto.entity.Talent;
 import lombok.Data;
+
+import java.io.Serializable;
+
 /**
  * BFF集約処理2
  */
 @Data
 public class GroupClassDto2 implements Serializable {
-   private Talent talent;
-   private Long shukanShutsuenHonsu;
-   private Integer targetNentsuki;
-   private Integer targetShu;
+    private Talent talent;
+    private Long shukanShutsuenHonsu;
+    private Integer nentsuki;
+    private Integer shu;
 }
