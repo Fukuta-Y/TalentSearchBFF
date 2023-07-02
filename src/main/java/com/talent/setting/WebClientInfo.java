dbe162595b2b7c05ad506298ccb40c12008f78c2
Fukuta-Y <yufukuta1008@gmail.co.jp>
2023-07-02 13:37:32 +0900

    レビュー結果

--- src/main/java/com/talent/setting/WebClientInfo.java
+++ src/main/java/com/talent/setting/WebClientInfo.java
@@ -1,31 +1,40 @@
 package com.talent.setting;
 
-import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
-
 import com.model.ShukanTalentJoho;
 import com.model.YearMonthWeekStartEndJoho;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class WebClientInfo {
 
     public WebClient webClient;
-    
+
     public WebClientInfo(WebClient.Builder webClientBuilder) {
-    	 this.webClient = webClientBuilder.baseUrl("http://localhost:8082").build();
+        this.webClient = webClientBuilder.baseUrl("http://localhost:8082").build();
     }
-    
-	// BE「週間タレント別情報検索」へ接続の設定
+
+    // BE「週間タレント別情報検索」へ接続の設定
     public ShukanTalentJoho getShukanTalentJoho(Integer nentsuki, Integer shu, String talentName) {
         return this.webClient.get()
-                .uri(String.format("/shukanTalentJoho?nentsuki=%d&shu=%d&talentName=%s",nentsuki, shu, talentName))
+                .uri(uriBuilder -> uriBuilder
+                        .path("/shukanTalentJoho")
+                        .queryParam("nentsuki", nentsuki)
+                        .queryParam("shu", shu)
+                        .queryParam("talentName", talentName)
+                        .build())
                 .retrieve()
                 .bodyToMono(ShukanTalentJoho.class).block();
     }
+
     // BE「年月週の開始終了日付検索」へ接続の設定
     public YearMonthWeekStartEndJoho getYearMonthWeekStartEnd(Integer nentsuki, Integer shu) {
         return this.webClient.get()
-                .uri(String.format("/yearMonthWeekStartEnd?nentsuki=%d&shu=%d",nentsuki, shu))
+                .uri(uriBuilder -> uriBuilder
+                        .path("/yearMonthWeekStartEnd")
+                        .queryParam("nentsuki", nentsuki)
+                        .queryParam("shu", shu)
+                        .build())
                 .retrieve()
                 .bodyToMono(YearMonthWeekStartEndJoho.class).block();
     }
