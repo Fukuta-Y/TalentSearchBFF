3abdaf2c812b8d1c3b2de7b262c1add214d0d5ad
Fukuta-Y <yufukuta1008@gmail.co.jp>
2023-07-17 19:11:39 +0900

    不要文言

--- src/main/java/com/talent/service/ShukanTalentJohoBFFService.java
+++ src/main/java/com/talent/service/ShukanTalentJohoBFFService.java
@@ -55,6 +55,7 @@ public class ShukanTalentJohoBFFService {
         List<TOnAirKanri> modelOnAirKanriList = talentJoho.gettOnAirKanri();
         List<MTalent> modelTalentList = talentJoho.getmTalent();
         List<MProgram> modelProgramList = talentJoho.getmProgram();
+
         // (1) BE「週間タレント別情報検索」より取得したレスポンスで以下の条件でデータを絞る。
         // （別シート_タレント出演情報検索の結合イメージ　参照）
         // 以下はオンエア管理が設定されている場合のみ対応
@@ -63,12 +64,10 @@ public class ShukanTalentJohoBFFService {
             return response;
         }
         List<OnAirKanriInfoDto> OnairKanriInfoList = groupShori.setOnairKanriInfoList(modelTalentList, modelProgramList, modelOnAirKanriList);
-		System.out.print("OnairKanriInfoList:" + OnairKanriInfoList);
         // (2) 絞った結果をタレントID、タレント名で集約化する。集約時に、タレントID,タレント名、週間出演番組本数のレコードの形にする。
         // （レスポンスのベース）
         List<TalentShutsuenHonsuDto> talentShutsuenHonsuList = groupShori.setTalentShutsuenHonsuList(OnairKanriInfoList);
         List<TalentShutsuenHonsuDto> talentShutsuenHonsuCountList = groupShori.setTalentShutsuenHonsuCountList(talentShutsuenHonsuList);
-		System.out.print("talentShutsuenHonsuCountList:" + talentShutsuenHonsuCountList);
         // (3)　(1)よりタレントIDをキーとして取得して、オンエア日でソートして、最も近いオンエア日の日付の行だけを取得する。
         // 取得後、タレントID、出演番組（直近）【番組名】、オンエア日（直近）【オンエア日】のレコードの形にする。
         List<String> talentList = new ArrayList<String>();
