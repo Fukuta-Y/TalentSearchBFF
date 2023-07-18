b9bfffdbdcead220b7740ddf9741055e66b00aba
Fukuta-Y <yufukuta1008@gmail.co.jp>
2023-07-18 16:22:47 +0900

    BFF「番組出演者検索BFF」の作成

--- src/main/java/com/talent/service/ProgramShutsuenBFFService.java
+++ src/main/java/com/talent/service/ProgramShutsuenBFFService.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.model.ProgramShutsuen;
 import com.model.ProgramShutsuenBFF;
 import com.model.ProgramShutsuenList;
 import com.model.YearMonthWeekStartEndJoho;
@@ -32,28 +33,28 @@ public class ProgramShutsuenBFFService {
      */
     public List<ProgramShutsuenBFF> select(String programId, String onairDay, Integer nentsuki, Integer shu) {
 
-		// (1) BE「番組出演者検索」より取得したレスポンスを以下のように設定する。
-		// ・タレントID⇒　レスポンス.タレントID
-		// ・タレント名⇒　レスポンス.タレント名
-
+    	// reponseを宣言
         List<ProgramShutsuenBFF> response = new ArrayList<ProgramShutsuenBFF>();
-        ProgramShutsuenBFF bffModel = new ProgramShutsuenBFF();
-        
-        // BE「週間タレント別情報検索より取得処理
-        ProgramShutsuenList talentJoho = this.webClient.getProgramShutsuen(programId, onairDay);
-        
-        bffModel.setTalentId(talentJoho.getProgramShutsuen().get(0).getTalentId());
-        bffModel.setTalentName(talentJoho.getProgramShutsuen().get(0).getTalentName());
-        
-		// (2) BE「年月週の開始終了日付検索」より取得したレスポンスを以下のように設定する。
-		// ・対象週(FROM)へ、年月週管理マスタDTO .週の開始日（日曜日）を設定
-		// ・対象週(TO)へ、年月週管理マスタDTO .週の終了日（土曜日）を設定
+    	
+		// (1) BE「年月週の開始終了日付検索」より取得
         YearMonthWeekStartEndJoho yearMonthJoho = this.webClient.getYearMonthWeekStartEnd(nentsuki, shu);
-        bffModel.setShuFrom(yearMonthJoho.getmNentsukiShuKanri().getShuFrom());
-        bffModel.setShuTo(yearMonthJoho.getmNentsukiShuKanri().getShuTo());
-
-        response.add(bffModel);
-        
+    	
+		// (2) BE「番組出演者検索」より取得
+        ProgramShutsuenList talentJoho = this.webClient.getProgramShutsuen(programId, onairDay);
+        for (ProgramShutsuen e : talentJoho.getProgramShutsuen()) {
+        	ProgramShutsuenBFF model = new ProgramShutsuenBFF();
+			// (1)で取得したレスポンスを以下のように設定する。
+			// ・対象週(FROM)へ、年月週管理マスタDTO .週の開始日（日曜日）を設定
+			// ・対象週(TO)へ、年月週管理マスタDTO .週の終了日（土曜日）を設定
+        	model.setShuFrom(yearMonthJoho.getmNentsukiShuKanri().getShuFrom());
+        	model.setShuTo(yearMonthJoho.getmNentsukiShuKanri().getShuTo());
+			// (2)で取得したレスポンスを以下のように設定する。
+    		// ・タレントID⇒　レスポンス.タレントID
+    		// ・タレント名⇒　レスポンス.タレント名
+        	model.setTalentId(e.getTalentId());
+        	model.setTalentName(e.getTalentName());
+	        response.add(model);
+        }
         // responseの返却
         return response;
     }
